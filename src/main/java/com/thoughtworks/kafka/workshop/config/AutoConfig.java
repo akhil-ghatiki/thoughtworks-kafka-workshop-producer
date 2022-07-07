3cfa058e73e9e3ff90d423c77de369e9f6f46110
akhil-ghatiki <akhil.ghatiki@gmail.com>
2022-07-07 09:45:20 +0530

    Add admin configuration to create topics

---
+++ src/main/java/com/thoughtworks/kafka/workshop/config/AutoConfig.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.kafka.workshop.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+@Profile("local")
+public class AutoConfig {
+
+  /**
+   * The profile is set to local only This auto config should never be used in production
+   * environment
+   */
+  @Bean
+  public NewTopic marketPlaceEvents() {
+    return TopicBuilder.name("market-place-events").partitions(3).replicas(3).build();
+  }
+}
