e35e6f8c6351fc6cc38d908e42980fbef0eec393
akhil-ghatiki <akhil.ghatiki@gmail.com>
2022-07-09 16:51:18 +0530

    Produce messages to kafka from controller

--- src/main/java/com/thoughtworks/kafka/workshop/controller/MarketPlaceController.java
+++ src/main/java/com/thoughtworks/kafka/workshop/controller/MarketPlaceController.java
@@ -1,19 +1,27 @@
 package com.thoughtworks.kafka.workshop.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.thoughtworks.kafka.workshop.data.MarketPlaceEvent;
+import com.thoughtworks.kafka.workshop.producer.MarketPlaceEventProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/market-place")
+@RestController
+@RequestMapping("/market-place")
 public class MarketPlaceController {
+
+  @Autowired MarketPlaceEventProducer marketPlaceEventProducer;
 
   @PostMapping("/event")
   public ResponseEntity<MarketPlaceEvent> postMarketPlaceEvent(
-      @RequestBody MarketPlaceEvent marketPlaceEvent) {
-    // publish the message to kafka broker
+      @RequestBody MarketPlaceEvent marketPlaceEvent) throws JsonProcessingException {
+
+    marketPlaceEventProducer.sendMarketPlaceEvent(marketPlaceEvent);
     return ResponseEntity.status(HttpStatus.CREATED).body(marketPlaceEvent);
   }
 }
