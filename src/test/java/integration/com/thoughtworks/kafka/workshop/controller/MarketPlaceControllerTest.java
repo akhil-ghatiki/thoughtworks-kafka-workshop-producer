fea1cac3d0a73c79bfc93fe59a7f37cb627910fc
akhil-ghatiki <akhil.ghatiki@gmail.com>
2022-07-11 00:06:45 +0530

    Evaluate produced payload using a consumer in intgration test

--- src/test/java/integration/com/thoughtworks/kafka/workshop/controller/MarketPlaceControllerTest.java
+++ src/test/java/integration/com/thoughtworks/kafka/workshop/controller/MarketPlaceControllerTest.java
@@ -4,12 +4,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.thoughtworks.kafka.workshop.data.MarketPlaceEvent;
 import com.thoughtworks.kafka.workshop.data.Product;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -20,10 +32,27 @@ import org.springframework.test.context.TestPropertySource;
     properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}"})
 public class MarketPlaceControllerTest {
 
-  @Autowired TestRestTemplate testRestTemplate;
+  @Autowired private TestRestTemplate testRestTemplate;
+
+  @Autowired EmbeddedKafkaBroker embeddedKafkaBroker;
+
+  private Consumer<Integer, String> consumer;
+
+  @BeforeEach
+  void setup() {
+    Map<String, Object> consumerConfig =
+        new HashMap<>(KafkaTestUtils.consumerProps("testGroup", "true", embeddedKafkaBroker));
+
+    consumer =
+        new DefaultKafkaConsumerFactory<>(
+                consumerConfig, new IntegerDeserializer(), new StringDeserializer())
+            .createConsumer();
+    embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+  }
 
   @Test
-  public void postMarketPlaceEvent() {
+  @Timeout(6)
+  public void postMarketPlaceEvent() throws InterruptedException {
     Product product =
         Product.builder()
             .productId(3)
@@ -41,5 +70,17 @@ public class MarketPlaceControllerTest {
             "/market-place/event", HttpMethod.POST, httpRequest, MarketPlaceEvent.class);
 
     assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+    ConsumerRecord<Integer, String> consumerRecord =
+        KafkaTestUtils.getSingleRecord(consumer, "market-place-events");
+    String value = consumerRecord.value();
+    String expectedValue =
+        "{\"marketPlaceEventId\":9,\"product\":{\"productId\":3,\"productName\":\"integration-name\",\"productBrand\":\"integration-brand\"}}";
+    assertEquals(expectedValue, value);
+  }
+
+  @AfterEach
+  public void tearDown() {
+    consumer.close();
   }
 }
