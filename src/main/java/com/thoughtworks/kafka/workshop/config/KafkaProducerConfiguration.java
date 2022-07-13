4b7cb1901a380e64e32363d065a4b7adc9773294
akhil-ghatiki <akhil.ghatiki@gmail.com>
2022-07-13 08:28:12 +0530

    Use back the yml config

--- src/main/java/com/thoughtworks/kafka/workshop/config/KafkaProducerConfiguration.java
+++ src/main/java/com/thoughtworks/kafka/workshop/config/KafkaProducerConfiguration.java
@@ -1,44 +1,37 @@
 package com.thoughtworks.kafka.workshop.config;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration // Enable this to take configuration from factory
+// @Configuration // Enable this to take configuration from factory
 public class KafkaProducerConfiguration {
 
-    /** This should not be used when the auto config is used. Refer AutoConfig.java
-     * Use either AutoConfig / producer factory.
-     * */
+  /**
+   * This should not be used when the auto config is used. Refer AutoConfig.java Use either
+   * AutoConfig / producer factory.
+   */
+  @Bean
+  public ProducerFactory<Integer, String> producerFactory() {
+    Map<String, Object> properties = new HashMap<>();
+    properties.put(
+        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+    properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+    properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    properties.put(ProducerConfig.ACKS_CONFIG, "all");
+    properties.put(ProducerConfig.RETRIES_CONFIG, 5);
+    properties.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, 300);
+    return new DefaultKafkaProducerFactory<>(properties);
+  }
 
-    @Bean
-    public ProducerFactory<Integer, String> producerFactory() {
-        Map<String,Object> properties = new HashMap<>();
-        properties.put(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092,localhost:9093,localhost:9094");
-        properties.put(
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                IntegerSerializer.class);
-        properties.put(
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
-        properties.put(ProducerConfig.ACKS_CONFIG,"all");
-        properties.put(ProducerConfig.RETRIES_CONFIG,5);
-        properties.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG,300);
-        return new DefaultKafkaProducerFactory<>(properties);
-    }
-
-    @Bean
-    public KafkaTemplate<Integer, String> kafkaTemplate(){
-        return new KafkaTemplate<>(producerFactory());
-    }
+  //    @Bean
+  public KafkaTemplate<Integer, String> kafkaTemplate() {
+    return new KafkaTemplate<>(producerFactory());
+  }
 }
--- src/test/java/unit/com/thoughtworks/kafka/workshop/controller/MarketPlaceControllerUnitTest.java
+++ src/test/java/unit/com/thoughtworks/kafka/workshop/controller/MarketPlaceControllerUnitTest.java
@@ -1,5 +1,10 @@
 package com.thoughtworks.kafka.workshop.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.thoughtworks.kafka.workshop.data.MarketPlaceEvent;
 import com.thoughtworks.kafka.workshop.data.Product;
@@ -11,33 +16,27 @@ import org.mockito.Mock;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
 @ExtendWith(SpringExtension.class)
 public class MarketPlaceControllerUnitTest {
 
-    @Mock
-    MarketPlaceEventProducer marketPlaceEventProducer;
-    @InjectMocks MarketPlaceController marketPlaceController;
+  @Mock MarketPlaceEventProducer marketPlaceEventProducer;
+  @InjectMocks MarketPlaceController marketPlaceController;
 
-    @Test
-    public void shouldReturnHttpStatusCreated() throws JsonProcessingException {
-        Product product =
-                Product.builder()
-                        .productId(3)
-                        .productName("integration-name")
-                        .productBrand("integration-brand")
-                        .build();
-        MarketPlaceEvent marketPlaceEvent =
-                MarketPlaceEvent.builder().marketPlaceEventId(9).product(product).build();
+  @Test
+  public void shouldReturnHttpStatusCreated() throws JsonProcessingException {
+    Product product =
+        Product.builder()
+            .productId(3)
+            .productName("integration-name")
+            .productBrand("integration-brand")
+            .build();
+    MarketPlaceEvent marketPlaceEvent =
+        MarketPlaceEvent.builder().marketPlaceEventId(9).product(product).build();
 
-        HttpStatus httpStatus = marketPlaceController.postMarketPlaceEvent(marketPlaceEvent).getStatusCode();
+    HttpStatus httpStatus =
+        marketPlaceController.postMarketPlaceEvent(marketPlaceEvent).getStatusCode();
 
-        verify(marketPlaceEventProducer,times(1)).sendMarketPlaceEvent(any());
-        assertEquals(HttpStatus.CREATED, httpStatus);
-    }
-
+    verify(marketPlaceEventProducer, times(1)).sendMarketPlaceEvent(any());
+    assertEquals(HttpStatus.CREATED, httpStatus);
+  }
 }
